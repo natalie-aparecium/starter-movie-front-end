683379519000c37b59391de67ab7d9f7b6d0040e
natalie palacios <natalie.pm82@outlook.com>
2022-09-27 15:29:59 -0700

    revert

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "https://serene-shelf-23744.herokuapp.com/";
+  process.env.REACT_APP_API_BASE_URL || "https://serene-shelf-23744.herokuapp.com/";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
